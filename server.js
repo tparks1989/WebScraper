3853805c6668d1ee0a19b510acf61c99ab703c77
TP <tparks1989@yahoo.com>
2019-05-28 23:16:48 -0400

    five

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var mongoose = require("mongoose");
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
 
@@ -19,6 +20,16 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(db, function (error) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log("mongoose connection is successful");
+    }
+});
 
 app.listen(PORT, function () {
     console.log("Listening on port:" + PORT);
