0090fd0a6637f44bf71f2c24bb6c9db390555770
TP <tparks1989@yahoo.com>
2019-05-30 14:32:16 -0400

    sixteen

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,7 @@
+var scrape = require("../scripts/scrape");
+var headlinesController = require("../controllers/headlines");
+var notesController = require("../controllers/notes")
+
 module.exports = function (router) {
     router.get("/", function (req, res) {
         res.render("home");
--- controllers/notes.js
+++ controllers/notes.js
@@ -9,17 +9,22 @@ module.exports = {
     },
     save: function (data, cb) {
         var newNote = {
-                _headlineID: data._id,
-                date: makeDate(),
-                noteText: data.noteText
-            },
-            Note.create(newNote, function (err, doc) {
-                if (err) {
-                    console.log(err)
-                } else {
-                    console.log(doc);
-                    cb(doc);
-                }
-            }),
+            _headlineID: data._id,
+            date: makeDate(),
+            noteText: data.noteText
+        };
+        Note.create(newNote, function (err, doc) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log(doc);
+                cb(doc);
+            }
+        });
     },
-}
+    delete: function (data, cb) {
+        NOte.remove({
+            _id: data._id
+        }, cb);
+    }
+};
